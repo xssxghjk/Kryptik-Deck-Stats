ec0dd433602743a68cc353fad3302774822ef953
enes <enes.findik@gmx.de>
2023-08-13 23:07:20 +0200

    feat: add title

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -15,6 +14,7 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <title>Kryptik Deck Stats</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
